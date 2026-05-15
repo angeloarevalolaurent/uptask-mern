862b8fdb83280c26183342edfb2b3a029f059132
Angelo Arevalo <arevaloangelomarcial@gmail.com>
2026-05-15 11:29:08 -0500

    [BACKEND] add terminal colors

--- uptask_backend/src/index.ts
+++ uptask_backend/src/index.ts
@@ -1,8 +1,10 @@
+import colors from "colors";
+
 import server from "./server";
 
 const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(colors.cyan.bold(`Server is running on port ${PORT}`));
 });
 
