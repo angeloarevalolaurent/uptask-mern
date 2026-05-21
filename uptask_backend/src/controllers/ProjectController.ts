120b1a85caeb27429933ae17c1076761dcce83b3
Angelo Arevalo <arevaloangelomarcial@gmail.com>
2026-05-21 16:33:27 -0500

    [BACKEND] implement create project endpoint with mongoose

--- uptask_backend/src/controllers/ProjectController.ts
+++ uptask_backend/src/controllers/ProjectController.ts
@@ -1,6 +1,18 @@
 import type { Request, Response } from "express";
+import Project from "../models/Project";
 
 export class ProjectController {
+
+    static createProjects = async (req: Request, res: Response) => {
+        const project = new Project(req.body)
+        
+        try {
+            await project.save()
+            res.send('Proyecto creado exitosamente')
+        } catch (error) {
+            res.status(500).json({ error: 'Error al crear el proyecto' })
+        }
+    }   
 
     static getAllProjects = async (req: Request, res: Response) => {
         res.send('Todos los proyectos')
