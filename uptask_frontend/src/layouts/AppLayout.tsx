8bc51d5096a7b70bf12564eac541bad527f14521
Angelo Arevalo <arevaloangelomarcial@gmail.com>
2026-05-26 12:04:04 -0500

    [FRONTEND] add headless ui responsive navigation menu

--- uptask_frontend/src/layouts/AppLayout.tsx
+++ uptask_frontend/src/layouts/AppLayout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom'
 import Logo from '@/components/Logo'
+import NavMenu from '@/components/NavMenu'
 
 export default function AppLayout() {
   return (
@@ -13,6 +14,7 @@ export default function AppLayout() {
             <Logo />
           </div>
 
+          <NavMenu />
           
         </div>
       </header>
