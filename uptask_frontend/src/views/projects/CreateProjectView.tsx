6559febc701bc911c4df32ee1ce278fb8e7aa3ad
Angelo Arevalo <arevaloangelomarcial@gmail.com>
2026-05-28 10:37:52 -0500

    [FRONTEND] create projects using TanStack Query mutations

--- uptask_frontend/src/views/projects/CreateProjectView.tsx
+++ uptask_frontend/src/views/projects/CreateProjectView.tsx
@@ -1,5 +1,6 @@
 import { Link, useNavigate } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
+import {useMutation} from '@tanstack/react-query'
 import {toast} from 'react-toastify'
 import ProjectForm from '@/components/projects/ProjectForm'
 import type { ProjectFormData } from '@/types/index'
@@ -19,11 +20,20 @@ export default function CreateProjectView() {
     defaultValues: initialValues
   })
 
-  const handleForm = async (formData: ProjectFormData) => {
-    const data = await createProject(formData)
-    toast.success(data)
-    navigate('/')
-  }
+  const {mutate} = useMutation({
+    mutationFn: createProject,
+      onError:()=> {
+
+      },
+
+      onSuccess: (data) => {
+        toast.success(data)
+        navigate('/')
+      }
+  })
+
+
+  const handleForm =(formData: ProjectFormData) => mutate(formData)
 
   return (
     <div className="space-y-10">
