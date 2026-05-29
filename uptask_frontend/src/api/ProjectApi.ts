0130f4f10f9f9cc2db880772fadf9dbca052a0c5
Angelo Arevalo <arevaloangelomarcial@gmail.com>
2026-05-29 09:58:18 -0500

    [FRONTEND] fetch project by id with react query

--- uptask_frontend/src/api/ProjectApi.ts
+++ uptask_frontend/src/api/ProjectApi.ts
@@ -1,7 +1,8 @@
 import api from '@/lib/axios'
-import type {ProjectFormData} from '@/types/index'
-import {isAxiosError} from 'axios'
+import type {Project ,ProjectFormData} from '@/types/index'
 import {dashboardProjectSchema} from '@/types/index'
+import {isAxiosError} from 'axios'
+
 
 export async function createProject(fromData: ProjectFormData) {
     
@@ -25,6 +26,20 @@ export async function getProjects() {
             return response.data 
         }
        
+    }catch(error){
+        if(isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+    
+}
+
+
+export async function getProjectById(id: Project["_id"]) {
+    
+    try{
+        const {data} = await api.get(`/projects/${id}`)
+        return data
     }catch(error){
         if(isAxiosError(error) && error.response) {
             throw new Error(error.response.data.error)
--- uptask_frontend/src/views/projects/EditProjectView.tsx
+++ uptask_frontend/src/views/projects/EditProjectView.tsx
@@ -1,12 +1,18 @@
 import {useParams} from "react-router-dom"
+import {useQuery} from '@tanstack/react-query'
+import {getProjectById} from '@/api/ProjectApi'
 
 export default function EditProjectView() {
 
     const params = useParams()
     const projectId = params.projectId!
 
-    console.log(projectId)
+  //  console.log(projectId)
 
+    const {data, isLoading} = useQuery({
+    queryKey: ['editProject', projectId],
+    queryFn: ( ) => getProjectById(projectId)
+  })
   return (
     <>
       <div>Editar Proyecto: {projectId}</div>
