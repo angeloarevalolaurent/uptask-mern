421b26377861d6c1a26e942706d9e73010af4d9c
Angelo Arevalo <arevaloangelomarcial@gmail.com>
2026-05-30 10:49:43 -0500

    feat: create project details page with react query and add task modal

--- uptask_frontend/src/views/projects/ProjectDetailsView.tsx
+++ uptask_frontend/src/views/projects/ProjectDetailsView.tsx
@@ -1,10 +1,30 @@
+import {  Navigate,useParams} from "react-router-dom"
+import { useQuery } from "@tanstack/react-query"
+import { getProjectById } from "@/api/ProjectApi"
+import ProjectHeader from "@/components/projects/project/ProjectHeader"
 
 
 export default function ProjectDetailsView() {
 
-  return (
+  const params = useParams()
+  const projectId= params.projectId!
+  // 1. QUERY
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["editProject", projectId],
+    queryFn: () => getProjectById(projectId),
+    retry: false
+  })
+
+    if(isLoading) return 'Cargando proyecto...'
+    if(isError) return <Navigate to="/404" />
+
+    if(data) return (
     <>
-      <div>Project Details</div>
+        <ProjectHeader 
+            data={data}
+        />
+
+        
     </>
   )
 }
