cb555ab003991963c0e06a6e4d0169d0496367f6
Angelo Arevalo <arevaloangelomarcial@gmail.com>
2026-06-02 11:41:16 -0500

    [FRONTEND] feat: mostrar estado traducido de tareas por columna

--- uptask_frontend/src/components/projects/task/TaskList.tsx
+++ uptask_frontend/src/components/projects/task/TaskList.tsx
@@ -20,6 +20,18 @@ const initialStatusGroups: GroupedTasks = {
 
 }
 
+
+
+const statusTranslations : { [key: string]: string } = {
+    pending: "Pendiente",
+    inProgress: "En Progreso",
+    onHold: "En Espera",
+    underReview: "En Revisión",
+    completed: "Completada"
+}
+
+
+
 export default function TaskList({ tasks }: TaskListProps) {
     const groupedTasks = tasks.reduce((acc, task) => {
     let currentGroup = acc[task.status] ? [...acc[task.status]] : [];
@@ -35,6 +47,12 @@ export default function TaskList({ tasks }: TaskListProps) {
             <div className='flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32'>
                 {Object.entries(groupedTasks).map(([status, tasks]) => (
                     <div key={status} className='min-w-[300px] 2xl:min-w-0 2xl:w-1/5'>
+                        
+                        <h3
+                            className={`capitalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8`}>
+                           {statusTranslations[status]}
+                        </h3>
+                        
                         <ul className='mt-5 space-y-5'>
                             {tasks.length === 0 ? (
                                 <li className="text-gray-500 text-center pt-3">No Hay tareas</li>
