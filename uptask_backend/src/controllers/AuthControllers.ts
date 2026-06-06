487aae2988843ef9fe480c43326161d9e2377d6f
Angelo Arevalo <arevaloangelomarcial@gmail.com>
2026-06-06 11:46:04 -0500

    [BACKEND] feat: implement create account controller

--- uptask_backend/src/controllers/AuthControllers.ts
+++ uptask_backend/src/controllers/AuthControllers.ts
@@ -1,10 +1,16 @@
 import type {Request, Response} from 'express';
-
+import User from '../models/user';
 
 export class AuthController {
 
     static createAccount = async (req: Request, res: Response) => {
-        res.send('desde /api/auth')
+        try{
+            const user = new User(req.body)
+            await user.save()
+            res.status(201).json({ message: 'Cuenta creada exitosamente' });
+        } catch (error) {
+            res.status(500).json({ error: 'Error al crear la cuenta' });
+        }
     }
 
 
