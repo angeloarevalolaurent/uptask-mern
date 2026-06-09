c060dfe09adc2e6628fac1c17d18fce884e3a027
Angelo Arevalo <arevaloangelomarcial@gmail.com>
2026-06-09 10:14:39 -0500

    [BACKEND] feat: hash user passwords with bcrypt before saving

--- uptask_backend/src/controllers/AuthControllers.ts
+++ uptask_backend/src/controllers/AuthControllers.ts
@@ -1,11 +1,17 @@
 import type {Request, Response} from 'express';
 import User from '../models/user';
+import { hashPassword } from '../utils/auth';
 
 export class AuthController {
 
     static createAccount = async (req: Request, res: Response) => {
         try{
+            const {password} = req.body
             const user = new User(req.body)
+        
+            // Hash the password before saving the user
+            user.password = await hashPassword(password)
+            
             await user.save()
             res.status(201).json({ message: 'Cuenta creada exitosamente' });
         } catch (error) {
