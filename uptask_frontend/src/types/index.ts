6c0c705a0c48ddf62770d640a5a1af938204567a
Angelo Arevalo <arevaloangelomarcial@gmail.com>
2026-06-10 17:50:56 -0500

    [FRONTEND] feat: add pin input component for account confirmation

--- uptask_frontend/src/types/index.ts
+++ uptask_frontend/src/types/index.ts
@@ -5,13 +5,15 @@ const authSchema = z.object({
     name: z.string(),
     email: z.email(),
     password: z.string(),
-    password_confirmation: z.string()
+    password_confirmation: z.string(),
+    token: z.string()
 })
 
 type Auth = z.infer< typeof authSchema>
 export type UserLoginForm = Pick<Auth, 'email'| 'password'>
 export type UserRegistrationForm = Pick<Auth, 'name'| 'email'|'password'|'password_confirmation' >
 
+export type ConfirmToken = Pick<Auth, 'token'>
 /** Task  */
 export const taskStatusSchema = z.enum(['pending', 'inProgress', 'onHold', 'underReview','completed'])
 export type TaskStatus = z.infer<typeof taskStatusSchema>
