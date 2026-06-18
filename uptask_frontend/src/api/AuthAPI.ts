da9b04512ffa89980d0e1387b8e3c524192a447f
Angelo Arevalo <arevaloangelomarcial@gmail.com>
2026-06-17 20:51:46 -0500

    [AUTH] feat: connect forgot password form with backend

--- uptask_frontend/src/api/AuthAPI.ts
+++ uptask_frontend/src/api/AuthAPI.ts
@@ -1,6 +1,6 @@
 import api from "@/lib/axios";
 import { isAxiosError } from "axios";
-import type { ConfirmToken, RequestConfirmationCodeForm, UserLoginForm, UserRegistrationForm } from "../types";
+import type { ConfirmToken, ForgotPasswordForm, RequestConfirmationCodeForm, UserLoginForm, UserRegistrationForm } from "../types";
 
 
 export async function createAccount(formData:UserRegistrationForm) {
@@ -59,3 +59,15 @@ export async function authenticateUser(formData:UserLoginForm) {
 }
 
 
+export async function forgotPassword(formData:ForgotPasswordForm) {
+    try {
+        const url = '/auth/forgot-password'
+        const {data} = await api.post<string>(url, formData)
+        return data
+
+    } catch (error) {
+        if (isAxiosError(error) && error.message) {
+            throw new Error(error.response?.data.error)
+        }
+    }
+}
