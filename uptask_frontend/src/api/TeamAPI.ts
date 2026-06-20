580b37ddebbd42a07b6d0ae3048b8cf52f640a32
Angelo Arevalo <arevaloangelomarcial@gmail.com>
2026-06-19 20:39:20 -0500

    [FRONTEND] feat: add users to project team

--- uptask_frontend/src/api/TeamAPI.ts
+++ uptask_frontend/src/api/TeamAPI.ts
@@ -1,6 +1,6 @@
 import { isAxiosError } from "axios";
 import api from "@/lib/axios";
-import type { Project, TeamMemberForm } from "../types";
+import type { Project, TeamMember, TeamMemberForm } from "../types";
 
 
 export async function findUserByEmail({projectId, formData} : {projectId: Project['_id'], formData: TeamMemberForm}) {
@@ -8,6 +8,24 @@ export async function findUserByEmail({projectId, formData} : {projectId: Projec
     try {
         const url =`/projects/${projectId}/team/find`
         const {data} = await api.post(url, formData)
+        console.log(data);
+        return data
+
+
+    } catch (error) {
+        if (isAxiosError(error)  && error.response) {
+            throw new Error(error.response.data.error); 
+        }
+    }
+
+}
+
+
+export async function addUserToProject({projectId, id} : {projectId: Project['_id'], id: TeamMember['_id']}) {
+
+    try {
+        const url =`/projects/${projectId}/team`
+        const {data} = await api.post(url, {id})
         console.log(data);
         return data
 
