99ac5a29dde9fd1875e75e0ac2a5da1ecb13a848
Angelo Arevalo <arevaloangelomarcial@gmail.com>
2026-06-25 17:48:25 -0500

    style: add visual drag effect to droppable areas

--- uptask_frontend/src/components/projects/task/DropTask.tsx
+++ uptask_frontend/src/components/projects/task/DropTask.tsx
@@ -9,9 +9,14 @@ export default function DropTask({status} : DropTaskProps) {
     const {isOver, setNodeRef} = useDroppable({
         id: status
     })
+
+    const style = {
+        opacity: isOver ? 0.4 : undefined
+    }
   return (
     <>
       <div
+            style={style}
             ref= {setNodeRef}
         className="text-xs font-semibold uppercase p-2 border border-dashed border-slate-500
                     mt-5 grid place-content-center text-slate-500"   
