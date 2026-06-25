6340f2026529c5b609653db2295bc2575ffaa84b
Angelo Arevalo <arevaloangelomarcial@gmail.com>
2026-06-24 21:19:37 -0500

    feat: create user profile form with react hook form

--- uptask_frontend/src/types/index.ts
+++ uptask_frontend/src/types/index.ts
@@ -30,6 +30,7 @@ export const userSchema = authSchema.pick({
 })
 
 export type User = z.infer<typeof userSchema>
+export type UserProfileForm = Pick<User, 'name' | 'email'>
 
 /**Notes */
 const noteSchema = z.object({
