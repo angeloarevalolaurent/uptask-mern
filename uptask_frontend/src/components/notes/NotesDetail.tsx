68fc0029b21899ef312ce3f4668166c3129d27d8
Angelo Arevalo <arevaloangelomarcial@gmail.com>
2026-06-24 19:51:41 -0500

    feat: show delete action only for note owner

--- uptask_frontend/src/components/notes/NotesDetail.tsx
+++ uptask_frontend/src/components/notes/NotesDetail.tsx
@@ -1,5 +1,7 @@
 import type { Note } from "@/types/index"
 import { formatDate } from "@/utils/utils"
+import { useAuth } from "../../hooks/useAuth"
+import { useMemo } from "react"
 
 
 type NoteDetailProps = {
@@ -8,17 +10,30 @@ type NoteDetailProps = {
 
 export default function NotesDetail({note} : NoteDetailProps) {
 
+    const {data, isLoading} = useAuth()
+
+    const canDelete = useMemo(() => data?._id === note.createdBy._id, [data])
+
+    if (isLoading) return 'Cargando....'
+
   return (
     <>
         <div className="p-3 flex justify-between items-center">
             <div>
                 <p>{note.content} por: <span className="font-bold">{note.createdBy.name}</span></p>
+            
+                <p className="text-xs text-slate-500">
+                    {formatDate(note.createdAt)}
+                </p>
             </div>
-            <p className="text-xs text-slate-500">
-                {formatDate(note.createdAt)}
-            </p>
-        </div>
       
+        {canDelete && (
+            <button
+                type="button"
+                className="bg-red-400 hover:bg-red-500 p-2 text-xs text-white font-bold cursor-pointer transition-colors"
+            >Eliminar</button>
+        )}
+       </div> 
     </>
   )
 }
