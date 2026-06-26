5a28b399673e04981f027f7b05093b4a29a47ddf
Angelo Arevalo <arevaloangelomarcial@gmail.com>
2026-06-26 18:31:45 -0500

    feat: cascade delete tasks and notes when deleting projects

--- uptask_backend/src/models/Project.ts
+++ uptask_backend/src/models/Project.ts
@@ -1,6 +1,7 @@
 import mongoose, {Schema, Document, PopulatedDoc, Types} from "mongoose";
-import {ITask} from "./Task";
+import Task, {ITask} from "./Task";
 import { IUser } from "./user";
+import Note from "./Note";
 
 export interface IProject extends Document {
     projectName: string;
@@ -44,6 +45,23 @@ const ProjectSchema : Schema = new Schema({
 }, {
     timestamps: true
 })
+
+
+/** Middleware */
+
+ProjectSchema.pre('deleteOne', {document: true}, async function() {
+    const projectId = this._id
+    if (!projectId) return
+
+    const tasks = await Task.find({ project : projectId})
+    for (const task of tasks ) {
+         await Note.deleteMany({ task: task.id})   
+    }
+    await Project.deleteMany({project: projectId})
+    console.log(this._id);
+    
+})
+
 
 const Project = mongoose.model<IProject>("Project", ProjectSchema)
 
